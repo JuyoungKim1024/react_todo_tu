db8c0cbbf3dd9d82b9e7bc0f84069b28ff30b601
Kim Juyoung <op45672@gmail.com>
2026-03-18 15:26:19 +0900

    checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 
 function App() {
-    const [todos, setTodos] = useState(['공부하기', '청소하기', '운동하기'])
+    const [todos, setTodos] = useState([
+        { todo: '공부하기', completed: true },
+        { todo: '청소하기', completed: false },
+        { todo: '운동하기', completed: false },
+    ])
 
     const handleOnSubmit = (e) => {
         e.preventDefault()
@@ -10,31 +14,33 @@ function App() {
             alert('할 일을 입력하세요.')
             return
         }
-        setTodos([...todos, form[0].value])
+        setTodos([...todos, { todo: form[0].value, completed: false }])
+        form[0].value = ''
     }
-    const addTodo = () => {
-        setTodos([...todos])
-    }
+
     const removeTodo = (index) => {
-        const newTodos = [...todos]
-        newTodos.splice(index, 1)
-        setTodos(newTodos)
+        const newState = todos.filter((_, i) => i !== index)
+        setTodos(newState)
     }
+    const toggleTodo = (index) => {
+        const newState = todos.map((item, i) => (item, i === index ? { ...item, completed: !item.completed } : item))
+        setTodos(newState)
+    }
+
     return (
         <>
             <h1>할 일 목록</h1>
             <form onSubmit={handleOnSubmit}>
                 <input type="text" placeholder="할 일을 입력하세요" />
-                <button type="submit" onClick={addTodo}>
-                    입력
-                </button>
+                <button type="submit">입력</button>
             </form>
 
-            <ul>
+            <ul style={{ padding: 0 }}>
                 {todos.map((item, index) => (
-                    <li key={index}>
-                        {item}
-                        <button onClick={() => removeTodo(index)}>삭제</button>
+                    <li key={index} style={{ listStyle: 'none' }}>
+                        {JSON.stringify(item.completed)}
+                        <input type="checkbox" checked={item.completed} onChange={() => toggleTodo(index)} />/
+                        {item.todo}/<button onClick={() => removeTodo(index)}>삭제</button>
                     </li>
                 ))}
             </ul>
