c24e95bb93bac32588999e2f59d850c146545c7a
Kim Juyoung <op45672@gmail.com>
2026-03-18 15:55:08 +0900

    간단한꾸미기

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,10 @@ function App() {
 
             <ul style={{ padding: 0 }}>
                 {todos.map((item) => (
-                    <li key={item.id} style={{ listStyle: 'none' }}>
+                    <li
+                        key={item.id}
+                        style={{ listStyle: 'none', textDecoration: item.completed ? 'line-through' : 'none' }}
+                    >
                         id: {item.id}/{JSON.stringify(item.completed)}
                         <input type="checkbox" checked={item.completed} onChange={() => toggleTodo(item.id)} />/
                         {item.todo}/<button onClick={() => removeTodo(item.id)}>삭제</button>
