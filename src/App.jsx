7b7311451ed48d4894b8b4d6ddf5275afc2f0621
Kim Juyoung <op45672@gmail.com>
2026-03-18 14:21:30 +0900

    리스트 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react'
+
 function App() {
-    return <>리액트</>
+    const [Todo, setTodo] = useState(['공부하기', '청소하기', '운동하기'])
+
+    const addTodo = () => {
+        setTodo([...Todo, '새로운 할 일'])
+    }
+    return (
+        <>
+            <h1>할 일 목록</h1>
+            <ul>
+                {Todo.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+            <button onClick={addTodo}>할 일 추가</button>
+        </>
+    )
 }
 
 export default App
